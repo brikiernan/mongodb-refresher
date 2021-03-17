73b4937a0955d4a016e5198dde0409ff9bdc42f0
Brian Kiernan <brian@briankiernan.com>
2021-03-17 11:51:49 -0400

    validation

--- users/test/update-test.js
+++ users/test/update-test.js
@@ -5,7 +5,7 @@ describe('updating records', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -38,5 +38,16 @@ describe('updating records', () => {
 
   it('a model class can find a record with an Id and update', done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('a user can have their postcount incremented by 1', done => {
+    // using an update operator
+    // https://docs.mongodb.com/manual/reference/operator/update/
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
---
+++ users/test/validation-test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('validating records', () => {
+  it("requires a user's name", () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required');
+  });
+
+  it("requires a user's name be longer than 2 characters", () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters');
+  });
+
+  it('disallows invaild record from being saved', done => {
+    const user = new User({ name: 'Al' });
+    user.save().catch(validationResult => {
+      const { message } = validationResult.errors.name;
+      assert(message === 'Name must be longer than 2 characters');
+      done();
+    });
+  });
+});
